8218614caccdf87155a7d029f9af5383fe32c223
Budd Hirons Jr <buddhironsjr@gmail.com>
2022-06-19 17:31:44 -0500

    first pass at parsing products, flattening variable products and image folders

--- converter.js
+++ converter.js
@@ -1,10 +1,17 @@
 import fs from 'fs'
 import csv from 'csv-parser'
-const titles = []
+import { debug } from 'console'
+import ustr from 'underscore.string'
+import yaml from 'yamljs'
+
+const names = []
+// date will be date of this invocation
+const importDate = new Date().toISOString();
 const sourcePath = './csv-file'
 const resultPath = './resulting-md-files'
+// set the target path for images, the image URLS will be flattened with the intent of all images imported here
+const imageDest = 'images/products/'
 const files = fs.readdirSync(sourcePath)
-
 
 // Create the resultPath in case it doesn't exist
 if (!fs.existsSync(resultPath)) {
@@ -13,20 +20,59 @@ if (!fs.existsSync(resultPath)) {
 
 // Use the object from csv-parser to create a hugo readable md
 function createMd(data) {
+    //prepare the product name for use as a filename
+    // capture my legacy SKU
+    let out = {}
+    let name = ustr.slugify(data.Name);
+    let sku = ustr.unquote(data['SKU'], "'");
+    let tags = [];
+    let images = [];
+    let content = ustr.clean(data['Description']);
+    // console.log(name);
+    // console.log(sku);
 
-    // Warn that duplicate titles have been overwritten
-    if (titles.includes(data.title_en))
-        console.warn(`${data.title_en} is a duplicate title and has been overwritten!`)
-    titles.push(data.title_en)
-
+    //my items contain duplicate names and I want to slugify the product name for use as a file name
+    //use this array to keep count of the dupes
+    // TODO consider options for handling variable product that is being flattened here
+    names.push(name)
+    let cnt = names.filter(nm => nm === name).length;
+    //if there is more than one, we will start adding -2, and -3, and so on
+    if(cnt > 1)
+        name = name.concat(`-${cnt}`);
+    
     // Parsing tags since they usually come in this format: "tag1, tag2, tag3"
-    if (data.tags) {
-        data.tags = data.tags.split(',')
-            .map(tag => tag.trim().replace(/  +/g, ' '))
+    if (data['Tags']) {
+        //console.log(data['Tags']);
+        //transform to an array of strings with single spaces and no surrounding quotes
+        tags = data['Tags']
+                .split(',')
+                .map(tag => ustr(tag).trim().unquote("'").decapitalize().value());
     }
 
+   // Parsing images which are also a comma list
+    if (data['Images']) {
+        //console.log(data['Images']);
+        images = data['Images']
+                 .split(',')
+                 .map(imgurl => imgurl.substring(imgurl.lastIndexOf('/') + 1))
+                 .map(imgurl => ustr.clean(imgurl))
+                 .map(imgurl => ustr(imageDest).concat(imgurl).value())
+    }
+
+    out.title = ustr.clean(data['Name']);
+    out.date = importDate;
+    out.enabled = false;
+    out.sku = sku;
+    out.description = ustr.clean(data['Short description']);
+    out.price = ustr.numberFormat(ustr.toNumber(data['Regular price'], 2), 2, ".", ",");
+    out.images = images;
+    //console.log(images);
+    out.tags = tags;
+    //console.log(tags);
+
     // Write file
-    fs.writeFileSync(`${resultPath}/${data.title_en}.md`, JSON.stringify(data, null, 2))
+    // we want our product hugo .md file with the yaml at the top for the front matter and the content below
+    fs.writeFileSync(`${resultPath}/${name}.md`, `---\r\n${yaml.stringify(out, null, 2)}\r\n---\r\n${content}`);
 }
 
 // This is to ignore the .gitkeep and allow multiple .csv
@@ -34,7 +80,10 @@ files.forEach(file => {
     if (file.split('.').at(-1) === 'csv') {
         fs.createReadStream(sourcePath + '/' + file)
             .pipe(csv())
-            .on('data', data => createMd(data))
+            .on('data', (row) => {
+                console.log(row);
+                createMd(row);
+            })
             .on('end', () => { console.log('DONE') })
     }
 })
